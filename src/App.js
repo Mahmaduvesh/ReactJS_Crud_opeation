1f378eca9564bbc7420aaa53a66a5c1080b5383b
Mahmaduvesh <uveshkhalifa7920@gmail.com>
2022-08-09 13:01:39 +0530

    print hello msg

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> hello this is crud operaion of reactjs</h2>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
